c0e50f12e46f9a36d4457d7fc5c7de36db95105b
Brunnokauan <brunno_kauan@hotmail.com>
2023-10-30 22:51:00 -0300

    Implementando tela de Login

--- msa_back/manager_msa/urls.py
+++ msa_back/manager_msa/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.welcome_view, name="welcome")
+    path("", views.welcome_view, name="welcome"),
+    path("login", views.login, name="login"),
 ]
--- msa_back/manager_msa/views.py
+++ msa_back/manager_msa/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def welcome_view(request):
     return render(request, template_name='welcome.html', status=200)
+
+def login(request):
+    return render(request, template_name='Auth/login.html', status=200)
