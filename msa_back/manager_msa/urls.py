15e63f7f1c12668f70d2e2a3fa999c3dfd551f64
Brunnokauan <brunno_kauan@hotmail.com>
2023-11-02 17:45:44 -0300

    Ajuste no registro e preparo para novas telas

--- msa_back/manager_msa/urls.py
+++ msa_back/manager_msa/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", views.welcome_view, name="welcome"),
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
+    path("main", views.main, name="main"),
+    path("catalogo", views.catalogo_geral, name="catalogo"),
+    path("infos/<int:video_id>", views.infos, name="infos"),
+    path("lista", views.lista_geral, name="lista"),
 ]
--- msa_back/manager_msa/views.py
+++ msa_back/manager_msa/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def welcome_view(request):
-    return render(request, template_name='welcome.html', status=200)
+    # return render(request, template_name='welcome.html', status=200)
+    return redirect('login')
 
 def login(request):
     return render(request, template_name='Auth/login.html', status=200)
 
 def register(request):
     return render(request, template_name='Auth/registro.html', status=200)
+
+def main(request):
+    return render(request, template_name='Videos/main.html', status=200)
+
+def catalogo_geral(request):
+    return render(request, template_name='Videos/catalogo.html', status=200)
+
+def lista_geral(request):
+    return render(request, template_name='Lista/lista.html', status=200)
+
+def infos(request, video_id):
+    return render(request, template_name='Videos/infos.html', status=200)
