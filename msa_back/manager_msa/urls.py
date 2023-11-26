2cc3694e720e9aed47943d8af0f13ddcbe9c616e
Brunnokauan <brunno_kauan@hotmail.com>
2023-11-26 18:47:13 -0300

    Novas telas e ajustes

--- msa_back/manager_msa/urls.py
+++ msa_back/manager_msa/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("main", views.main, name="main"),
     path("catalogo", views.catalogo_geral, name="catalogo"),
-    path("infos/<int:video_id>", views.infos, name="infos"),
+    path("infos/<int:video_id>/<str:type>", views.infos, name="infos"),
     path("lista/<int:user_id>", views.lista_geral, name="lista"),
     path("delete/<int:video_id>", views.delete_movie, name="delete-movie"),
+    path("rating/<int:video_id>/movie/<int:rating>", views.rating_movie, name="rating-movie"),
+    path('search', views.search, name='search'),
 ]
