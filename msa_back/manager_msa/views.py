b37f77f4af787e54747e8a5715b5aa4a48c1c1bb
Brunnokauan <brunno_kauan@hotmail.com>
2023-11-27 21:38:19 -0300

    Correção de commit

--- msa_back/manager_msa/views.py
+++ msa_back/manager_msa/views.py
@@ -9,6 +9,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
+from django.db.models import Q
+from datetime import datetime
 
 # Create your views here.
 def welcome_view(request):
@@ -52,9 +54,29 @@ def register(request):
 @login_required(login_url='/login')
 def main(request):
     resp_api = api.get(url="https://api.themoviedb.org/3/trending/all/day?language=pt-Br")
+    for r in resp_api['results']:
+        if r['media_type'] == 'tv' and r['first_air_date'] != '':
+            r['first_air_date_format'] = datetime.strptime(r['first_air_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
+        elif r['media_type'] == 'movie':
+            r['release_date_format'] = datetime.strptime(r['release_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
     movies = api.get(url="https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1")
+    # movies['results'][0]['release_date_format'] = datetime.strptime(movies['results'][0]['release_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
+    for m in movies['results']:
+        m['release_date_format'] = datetime.strptime(m['release_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
     series = api.get(url="https://api.themoviedb.org/3/tv/popular?language=pt-BR&page=1")
-    return render(request, template_name='Videos/main.html', status=200, context={'videos':resp_api, 'movies': movies, 'series': series})
+    for s in series['results']:
+        s['first_air_date_format'] = datetime.strptime(s['first_air_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
+    movies_now = api.get(url="https://api.themoviedb.org/3/movie/now_playing?language=pt-BR&page=1")
+
+    context = {
+        'videos': resp_api, 
+        'movies': movies, 
+        'series': series,
+        'movies_now': movies_now,
+        # 'date': movie_date_format,
+    }
+
+    return render(request, template_name='Videos/main.html', status=200, context=context)
 
 @login_required(login_url='/login')
 def catalogo_geral(request):
@@ -78,8 +100,9 @@ def lista_geral(request, user_id):
     for l in list_movie:
         movie = api.get(url=f"https://api.themoviedb.org/3/movie/{l.movie}?language=pt-BR")
         # movie['release_date'] = movie.release_date.strftime("%d/%m/%y")
-        movie['status_watch'] = l.watch
         movie['id_bd'] = l.id
+        movie['rating_personal'] = l.rating
+        movie['release_date_format'] = datetime.strptime(movie['release_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
         lista_filmes.append(movie)
 
         context = {
@@ -90,31 +113,55 @@ def lista_geral(request, user_id):
     return render(request, template_name='Lista/lista.html', context=context)
 
 @login_required(login_url='/login')
-def infos(request, video_id):
-    movie_detail = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}?language=pt-BR")
-    movie_credits = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}/credits?language=pt-BR")
-    movie_images = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}/images")
+def infos(request, video_id, type):
+
+    if type == 'movie':
+        video_detail = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}?language=pt-BR")
+        video_detail['type_video'] = 'movie'
+        video_credits = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}/credits?language=pt-BR")
+        video_images = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}/images")
+        
+    else:
+        video_detail = api.get(url=f"https://api.themoviedb.org/3/tv/{video_id}?language=pt-BR")
+        video_detail['type_video'] = 'serie'
+        video_credits = api.get(url=f"https://api.themoviedb.org/3/tv/{video_id}/credits?language=pt-BR")
+        video_images = api.get(url=f"https://api.themoviedb.org/3/tv/{video_id}/images?language=pt-BR")
+
     message = {
-            'msg': '',
-            'type': ''
-        }
-    movie = Movie(user_id= request.user.pk ,movie=video_id, watch=0)
+                'msg': '',
+                'type': ''
+            }
+    movie = Movie(user_id=request.user.pk ,movie=video_id, watch=0)
     if request.method == "POST":
         movie.save()
         
         message = {
-            'msg': f"Filme {movie_detail['title']} adicionado com sucesso na sua lista de filmes!",
+            'msg': f"Filme {video_detail['title']} adicionado com sucesso na sua lista de filmes!",
             'type': 'success'
         }
     
     context = {
-        'details': movie_detail,
+        'details': video_detail,
         'alert': message,
-        'credits': movie_credits,
-        'images': movie_images,
+        'credits': video_credits,
+        'images': video_images,
     }
         
     return render(request, template_name='Videos/infos.html', context=context)
+
+@login_required(login_url='/login')
+def rating_movie(request, rating, video_id):
+    # movie = Movie.objects.get(id=video_id)
+    movie_detail = api.get(url=f"https://api.themoviedb.org/3/movie/{video_id}?language=pt-BR")
+    movie = Movie.objects.filter(movie=video_id).update(rating=rating)
+    # message = {
+    #     'msg': f'Filme {movie_detail[0].title} deletado com sucesso!',
+    #     'type': 'success'
+    # }
+
+    # context = {'alert': message}
+    messages.success(request, f"Filme {movie_detail['title']} avaliado com sucesso!")
+    return HttpResponseRedirect(reverse('lista', kwargs={'user_id':request.user.id}))
 
 @login_required(login_url='/login')
 def delete_movie(request, video_id):
@@ -129,6 +176,31 @@ def delete_movie(request, video_id):
     # context = {'alert': message}
     messages.success(request, f"Filme {movie_detail['title']} deletado com sucesso!")
     return HttpResponseRedirect(reverse('lista', kwargs={'user_id':request.user.id}))
+
+@login_required(login_url='/login')
+def search(request):
+    resp_api = api.get(url="https://api.themoviedb.org/3/trending/all/week?language=pt-Br")
+
+    search_videos = request.GET.get('search')
+
+    if search_videos:
+        Q(search_videos)
+        videos = api.get(url=f"https://api.themoviedb.org/3/search/multi?query={search_videos}&include_adult=false&language=pt-BR&page=1")
+        for v in videos['results']:
+            if v['media_type'] == 'tv' and v['first_air_date'] != '':
+                v['first_air_date_format'] = datetime.strptime(v['first_air_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
+            elif v['media_type'] == 'movie':
+                v['release_date_format'] = datetime.strptime(v['release_date'], "%Y-%m-%d").strftime('%d/%m/%Y')
+    else:
+        videos = 'Não encontrado'
+    
+    context = {
+        'keywords': resp_api,
+        'search': videos,
+        'search_video': search_videos,
+    }
+
+    return render(request, template_name='search.html', context=context)
 
 def perguntas(request):
     perguntas = [
